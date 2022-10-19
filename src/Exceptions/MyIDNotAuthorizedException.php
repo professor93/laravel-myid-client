abadd074dde83638f8d414ebcd5ca45ec16932ca
Mr.Professor <mronx@ya.ru>
2022-10-19 22:03:23 +0500

    WIP: implementing authorization

---
+++ src/Exceptions/MyIDNotAuthorizedException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Uzbek\LaravelMyidClient\Exceptions;
+
+use Exception;
+
+class MyIDNotAuthorizedException extends Exception
+{
+}
---
+++ src/MyIDCompareFace.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Uzbek\LaravelMyidClient;
+
+class MyIDCompareFace
+{
+
+}
