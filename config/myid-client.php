d084319f29f0922b8f8c410cc077c0309b181a34
Sodiqmirzo Sattorov <mr.sodiqmirzosattorov17@gmail.com>
2022-10-20 13:23:38 +0500

    added username and password

--- config/myid-client.php
+++ config/myid-client.php
@@ -5,4 +5,6 @@ return [
     'base_url' => env('MYID_BASE_URL', 'https://faced.track.uz/api/v1/'),
     'client_id' => env('MYID_CLIENT_ID', 'client_id'),
     'client_secret' => env('MYID_CLIENT_SECRET', 'client_secret'),
+    'username' => env('MYID_USERNAME', 'username'),
+    'password' => env('MYID_PASSWORD', 'password'),
 ];
